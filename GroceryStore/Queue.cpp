540e5b389fd0e8d8303cf23bb13c57fda4dd7df5
Alyssa McMurry <ajmdfd@umsystem.edu>
2023-04-25 15:09:23 -0500

    Queue1
    my initial queue changes

--- GroceryStore/Queue.cpp
+++ GroceryStore/Queue.cpp
@@ -4,27 +4,87 @@ Queue::Queue()
 {
 	front = nullptr;
 	rear = nullptr;
+	int queueCount = 0; //current queue length (set to 0 initially)
+	int totalIdleTime = 0; //if queueCount == 0; this is incremented
+	int totalOverTime = 0; //increment if current time > 720 & there are people in line
+	int maxQueueLength = 0; //if current queueCount>maxQueueLength, reset
+	int currItems = 0; //update as customers are added/removed from queue
+	int totalItems = 0; //running count of items purchased
+	vector<int> cartList; //running list of carts helped
 }
 
 void Queue::enQueue(queueNodeData nodeData)
 {
+	queueNode* tempNode = new queueNode;
+	tempNode->data = nodeData;
+	tempNode->nextPtr = nullptr;
+	if (front == nullptr) {
+		front = tempNode;
+		rear = tempNode;
+	}
+	else {
+		rear->nextPtr = tempNode;
+		rear = tempNode;
+	}
+	queueCount++;
 }
 
 Queue::queueNodeData Queue::deQueue()
 {
-	return queueNodeData();
+	queueNodeData data;
+	if (front == nullptr) {
+		cout << "The Queue is empty" << endl;
+		return data;
+	}
+	// Store the data of the front node
+	data = front->data;
+
+	// Move the front pointer to the next node
+	queueNode* temp = front;
+	front = front->nextPtr;
+
+	// Check if the queue is now empty
+	if (front == nullptr) {
+		rear = nullptr;
+	}
+
+	// Delete the node that was removed
+	delete temp;
+
+	// Update the queue count and current item count
+	queueCount--;
+	currItems -= data.itemCount;
+	return data;
 }
 
 Queue::queueNodeData Queue::peek()
 {
-	return queueNodeData();
+	return front->data;
 }
 
 bool Queue::queueEmpty()
 {
-	return false;
+	if (front == nullptr)
+		return true;
+	else
+		return false;
 }
 
 void Queue::printQueue()
 {
+	queueNode* tempNode = front;
+	while (tempNode != nullptr) {
+		int queueCount = 0; //current queue length (set to 0 initially)
+		int totalIdleTime = 0; //if queueCount == 0; this is incremented
+		int totalOverTime = 0; //increment if current time > 720 & there are people in line
+		int maxQueueLength = 0; //if current queueCount>maxQueueLength, reset
+		int currItems = 0; //update as customers are added/removed from queue
+		int totalItems = 0; //running count of items purchased
+		cout << "Queue Count: " << tempNode->data.queueCount << endl;
+		cout << "Item Count: " << tempNode->data.itemCount << endl;
+		cout << "Enter Time: " << tempNode->data.enterQTime << endl;
+		cout << "Exit Time: " << tempNode->data.exitQTime << endl;
+		cout << endl;
+		tempNode = tempNode->nextPtr;
+	}
 }
